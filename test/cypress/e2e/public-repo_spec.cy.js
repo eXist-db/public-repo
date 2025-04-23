3b31dfff1f071b26e3a30d2921f59a566f4871ae
Juri Leino <github@line-o.de>
2025-04-23 16:39:25 +0200

    test: fix and expand e2e tests

--- test/cypress/e2e/public-repo_spec.cy.js
+++ test/cypress/e2e/public-repo_spec.cy.js
@@ -2,24 +2,81 @@
 /// <reference types="cypress" />
 
 context('expath package registry', () => {
-  beforeEach(() => {
-    cy.visit('')
-  })
+  const navBar = () => {
+    cy.get('.navbar').should('exist')
+    cy.get('.nav-link').should('have.length', 5)
+  };
 
   describe('landing page', () => {
-    it('should contain major parts', () => {
-      cy.get('.navbar')
-        .should('exist')
-      cy.get('.nav-link')
-        .should('have.length', '3')
-      cy.get('h1')
-        .contains('Package Registry')
-      cy.get('#package-list > h2')
-        .contains('Available Packages')
-      cy.get('.packages')
-        .should('exist')
-      cy.get('#user-docu > h2')
-        .contains('Info')
+    beforeEach(() => {
+      cy.visit('')
+    })
+
+    it('should contain a navigation bar', navBar)
+
+    it('should have the default main title', () => {
+      cy.get('h1.hero').contains('EXPath Package Registry')
+    })
+
+    it('should have an Installation section', () => {
+      cy.get('#installation > h2').contains('Installation')
     })
   })
+
+  describe('list page', () => {
+    beforeEach(() => {
+      cy.visit('list')
+    })
+
+    it('should contain a navigation bar', navBar)
+
+    it('should have the page title', () => {
+      cy.get('h1').contains('Available Packages')
+    })
+
+    it('should have an list of packages', () => {
+      cy.get('.package-list').should('exist')
+    })
+  })
+
+  describe('search page', () => {
+    beforeEach(() => {
+      cy.visit('search')
+    })
+
+    it('should contain a navigation bar', navBar)
+
+    it('should have the page title', () => {
+      cy.get('h1').contains('Package Search')
+    })
+  })
+
+  describe('admin page', () => {
+    beforeEach(() => {
+      cy.visit('admin')
+    })
+
+    it('should contain a navigation bar', navBar)
+
+    it('should show the login', () => {
+      cy.get('h1').contains('Administrator Login')
+    })
+
+    // TODO
+    // login
+    // upload
+  })
+
+  describe('stats page', () => {
+    beforeEach(() => {
+      cy.visit('stats')
+    })
+
+    it('should contain a navigation bar', navBar)
+
+    it('should have the page title', () => {
+      cy.get('h1').contains('Statistics')
+    })
+  })
+
 })
