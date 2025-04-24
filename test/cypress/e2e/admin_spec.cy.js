aaa0ec0f3552554399fa9ffb82341c267ad0d9e2
duncdrum <d.paterson@me.com>
2025-04-24 13:51:44 +0200

    test(e2e): upload and login
    TODO: The test xar is not cleanup up after installation
    
    see #98

--- test/cypress/e2e/admin_spec.cy.js
+++ test/cypress/e2e/admin_spec.cy.js
@@ -12,9 +12,35 @@ describe('admin page', () => {
 
     it('should show the login', () => {
         cy.get('h1').contains('Administrator Login')
+        cy.get('form').should('exist')
+        cy.get('input[name="user"]').should('exist')
+        cy.get('input[name="password"]').should('exist')
+        cy.get('button[type="submit"]').should('exist')
+    })
+    
+    it('unauthorized user should not see admin section', () => {
+        cy.login('blah', 'blah')
+        cy.get('h1').should('not.eq','Admin')
     })
 
-    // TODO
-    // login
-    // upload
+    it('authorized user should be able to upload package', () => {
+        cy.fixture('test-app.xar', null).as('test-app')
+        cy.login('repo', 'repo')
+        cy.url().should('include', '/admin')
+        cy.get('h1').contains('Admin')
+        cy.get('aside > h2').contains('Upload Packages')
+        cy.get('#files')
+          .selectFile('@test-app')
+        cy.get('#upload')
+          .click()
+        cy.get('#uploaded > tr > td')
+          .contains('test-app.xar')
+        cy.reload()
+        cy.get('.package-list >h3')
+          .contains('test-app')
+        cy.get('[href="?logout=true"]')
+          .click()
+        cy.url().should('not.include', '/admin')
+    })
+
 })
